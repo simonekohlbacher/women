620ba7183902ae3547798b3d372f717ac98a9f5e
Simone <151656886+simone013124@users.noreply.github.com>
2025-12-20 15:52:24 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+
+  <main>
+
+    <div class="overview flex items-center justify-center h-screen flex-col gap-10">
+      <h1 class="text-5xl">Benachteiligung von Frauen in Österreich</h1>
+      <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg xl:btn-xl bg-white rounded-3xl color-white">Lets go</button>
+    </div>
+    
+
+  </main>
+</template>
+
